0a0972b830bc38a56cab345b1e84a3eee60b59eb
John Lewicki <jlewicki@gmail.com>
2023-12-11 06:21:28 -0600

    Adjust Inspector to use a Stack

--- lib/src/widgets/inspector.dart
+++ lib/src/widgets/inspector.dart
@@ -14,20 +14,28 @@ class StateTreeInspector extends StatelessWidget {
   Widget build(BuildContext context) {
     var info = TreeStateMachineProvider.of(context);
     if (info != null) {
-      return Column(
+      return Stack(
         children: [
-          Container(
-              padding: const EdgeInsets.all(8),
-              color: Colors.blueGrey.shade200,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (info.currentState.stateMachine.label != null)
-                    Text('State Machine: ${info.currentState.stateMachine.label}'),
-                  Text("Active States: ${info.currentState.activeStates.join(' -> ')}"),
-                ],
-              )),
-          Expanded(child: child),
+          child,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.blueGrey.shade200.withOpacity(0.5),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (info.currentState.stateMachine.label != null)
+                      Text('State Machine: ${info.currentState.stateMachine.label}'),
+                    Text("Active States: ${info.currentState.activeStates.join(' -> ')}"),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          //Expanded(child: child),
         ],
       );
     }

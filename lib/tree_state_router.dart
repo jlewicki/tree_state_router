abf63ce531318da8b776cf2f8af49acd49d46add
John Lewicki <jlewicki@gmail.com>
2023-12-12 19:53:59 -0600

    Add comments

--- lib/tree_state_router.dart
+++ lib/tree_state_router.dart
@@ -7,3 +7,13 @@ export 'src/routes/data_routes.dart';
 export 'src/widgets/inspector.dart';
 export 'src/widgets/state_machine_provider.dart';
 export 'src/widgets/nested_router.dart';
+
+// To publish:
+// dart pub publish --dry-run
+// git tag -a vX.X.X -m "Publish vX.X.X"
+// git push origin vX.X.X
+// dart pub publish
+//
+// If you mess up
+// git tag -d vX.X.X
+// git push --delete origin vX.X.X
